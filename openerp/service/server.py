7dc8b392340e9eeb8a0c94909b97025883641631
E.R. Spada II <er@eugeconsulting.com>
2016-03-25 13:35:29 -0400

    [IMP] [8.0] [9.0] Odoo can't spawn workers in multiprocess mode in a virtual environment

--- openerp/service/server.py
+++ openerp/service/server.py
@@ -45,9 +45,8 @@ _logger = logging.getLogger(__name__)
 SLEEP_INTERVAL = 60     # 1 min
 
 def memory_info(process):
-    """ psutil < 2.0 does not have memory_info, >= 3.0 does not have
-    get_memory_info """
-    return (getattr(process, 'memory_info', None) or process.get_memory_info)()
+    pmem = process.memory_info()
+    return (pmem.rss, pmem.vms)
 
 #----------------------------------------------------------
 # Werkzeug WSGI servers patched
